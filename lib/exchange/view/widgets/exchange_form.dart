dfd150324c0d41429dd1eaa4947651c5e39c3828
geekfabio <geekfabio@gmail.com>
2022-06-24 22:45:41 +0100

    ✨ Ammount in Kwanza

--- lib/exchange/view/widgets/exchange_form.dart
+++ lib/exchange/view/widgets/exchange_form.dart
@@ -102,6 +102,46 @@ class _ExchangeFormState extends State<ExchangeForm> {
                 ),
               ],
             ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(6),
+            margin: const EdgeInsets.only(top: 5, left: 18, right: 18),
+            width: double.maxFinite,
+            height: 80,
+            decoration: BoxDecoration(
+              color: const Color.fromRGBO(27, 26, 28, 1),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                const Text(
+                  'Amount',
+                  style: TextStyle(color: Colors.white),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  children: const [
+                    Icon(
+                      Icons.currency_bitcoin,
+                      color: Colors.white,
+                    ),
+                    Text(
+                      'Digit here ammount',
+                      style: TextStyle(color: Colors.grey),
+                    ),
+                    Spacer(),
+                    Icon(
+                      Icons.arrow_back_ios_new,
+                      color: Colors.white,
+                    ),
+                  ],
+                ),
+              ],
+            ),
           )
         ],
       ),
