7ddb025c2f1828deee86684ece79763db90f1c61
geekfabio <geekfabio@gmail.com>
2022-07-03 20:19:34 +0100

    added txtField removed static text

--- lib/exchange/view/widgets/exchange_form.dart
+++ lib/exchange/view/widgets/exchange_form.dart
@@ -33,7 +33,7 @@ class _ExchangeFormState extends State<ExchangeForm> {
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
                 const Text(
-                  'Currency',
+                  'Moeda',
                   style: TextStyle(color: Colors.white),
                 ),
                 const SizedBox(
@@ -44,20 +44,17 @@ class _ExchangeFormState extends State<ExchangeForm> {
                     Icon(
                       Icons.currency_exchange_outlined,
                       color: Colors.white,
+                      size: 15,
                     ),
                     Text(
-                      'American Dollar',
+                      '  Dollar',
                       style: TextStyle(color: Colors.white),
                     ),
+                    Spacer(),
                     Text(
                       ' USD',
                       style: TextStyle(color: Colors.grey),
                     ),
-                    Spacer(),
-                    Icon(
-                      Icons.keyboard_arrow_down_outlined,
-                      color: Colors.white,
-                    )
                   ],
                 )
               ],
@@ -67,7 +64,7 @@ class _ExchangeFormState extends State<ExchangeForm> {
             padding: const EdgeInsets.all(6),
             margin: const EdgeInsets.only(top: 5, left: 18, right: 18),
             width: double.maxFinite,
-            height: 80,
+            height: 100,
             decoration: BoxDecoration(
               color: const Color.fromRGBO(27, 26, 28, 1),
               borderRadius: BorderRadius.circular(10),
@@ -77,29 +74,13 @@ class _ExchangeFormState extends State<ExchangeForm> {
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
                 const Text(
-                  'Amount',
+                  'Quantidade USD',
                   style: TextStyle(color: Colors.white),
                 ),
                 const SizedBox(
                   height: 10,
                 ),
-                Row(
-                  children: const [
-                    Icon(
-                      Icons.currency_bitcoin,
-                      color: Colors.white,
-                    ),
-                    Text(
-                      'Digit here ammount',
-                      style: TextStyle(color: Colors.grey),
-                    ),
-                    Spacer(),
-                    Icon(
-                      Icons.arrow_back_ios_new,
-                      color: Colors.white,
-                    ),
-                  ],
-                ),
+                Container(child: const TextField()),
               ],
             ),
           ),
@@ -115,30 +96,13 @@ class _ExchangeFormState extends State<ExchangeForm> {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                const Text(
+              children: const [
+                Text(
                   'Converted Value',
                   style: TextStyle(color: Colors.white),
                 ),
-                const SizedBox(
+                SizedBox(
                   height: 10,
-                ),
-                Row(
-                  children: const [
-                    Icon(
-                      Icons.currency_bitcoin,
-                      color: Colors.white,
-                    ),
-                    Text(
-                      'Ammount in Kwanza',
-                      style: TextStyle(color: Colors.grey),
-                    ),
-                    Spacer(),
-                    Icon(
-                      Icons.arrow_back_ios_new,
-                      color: Colors.white,
-                    ),
-                  ],
                 ),
               ],
             ),
