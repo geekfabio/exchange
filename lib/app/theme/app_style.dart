b287f12b2c275fef4504a79b27acb1f2f12974fd
geekfabio <geekfabio@gmail.com>
2022-07-06 23:45:40 +0100

    added AppStyle for const values

---
+++ lib/app/theme/app_style.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class AppStyle {
+  // ? Buttom style
+  static ButtonStyle primaryButton = ButtonStyle(
+    backgroundColor: MaterialStateProperty.all<Color>(
+      const Color.fromARGB(255, 32, 113, 122),
+    ),
+    minimumSize: MaterialStateProperty.all<Size>(const Size(200, 50)),
+  );
+
+  // ? Text Style
+  static const TextStyle textBody =
+      TextStyle(color: Color.fromARGB(255, 190, 190, 190));
+
+  static const TextStyle textPrimary = TextStyle(
+    color: Color.fromARGB(255, 78, 70, 70),
+    fontWeight: FontWeight.w600,
+  );
+}
